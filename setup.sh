677f6c9f15371001726bd8820ec7c5a2b0e7ca8a
Le Viet Hung (Jack Ryan) <leviethung1280@gmail.com>
2023-11-30 22:34:21 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+pip install transformers==4.35.0
+pip install torch==2.0.1
+pip install sentencepiece==0.1.99
+pip install tokenizers==0.13.3
+pip install accelerate==0.21.0
+pip install bitsandbytes==0.40.0
+pip install vllm
+pip install fraction
+pip install protobuf
+pip install loguru
+pip install scipy
+pip install peft
+pip install wandb
