c90961eb355678a778f3eaef74c289a3d43c3127
Le Viet Hung (Jack Ryan) <leviethung1280@gmail.com>
2023-11-30 22:37:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ pip install sentencepiece==0.1.99
 pip install tokenizers==0.13.3
 pip install accelerate==0.21.0
 pip install bitsandbytes==0.40.0
-pip install vllm
+# pip install vllm
 pip install fraction
 pip install protobuf
 pip install loguru
